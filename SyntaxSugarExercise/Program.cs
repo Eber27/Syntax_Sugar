1acd8a39c2cd7addeead8265529cb6232fa75df8
Eber27 <eberfreire@gmail.com>
2021-02-28 22:05:54 -0600

    SyntaxSugar

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+
+//namespace SyntaxSugarExercise
+//{
+//    class Program
+//    {
+       //static void Main(string[] args)
+        //{
+            //var answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //response = answer + " is less than nine";
+            //}
+
+            //else
+
+            //{
+            //response = answer + "greater than or equal to nine";
+            //}
+            //Console.WriteLine(response);
+
+            // in line if - Ternary Operator
+            //var answer = 4;
+            //var response = (answer >= 9) ? $"{answer} greater or equal to nine " : $"{answer} is less than nine";
+            //Console.WriteLine(response);
